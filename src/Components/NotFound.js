968640efc82ac3ee3332d98dd5c6f74ef76cd4a1
zsamamah <zsamamah@yahoo.com>
2021-11-27 15:11:13 +0200

    new edit

--- src/Components/NotFound.js
+++ src/Components/NotFound.js
@@ -5,7 +5,7 @@ export class NotFound extends Component {
     render() {
         return (
             <div className="noLogin">
-                <div><h1>Page not found!!</h1></div>
+                <div><h1>Page Not Found!!</h1></div>
             </div>
         )
     }
